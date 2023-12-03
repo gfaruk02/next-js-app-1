16333163de15fffa6a98ae489186cec03b645d24
gfaruk02 <gfaruk02@gmail.com>
2023-12-03 11:18:10 +0600

    first practice next js

---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+
+
+const page = () => {
+    return (
+        <div>
+            <h1>This is my about page </h1>
+        </div>
+    );
+};
+
+export default page;
---
+++ src/app/product/[id]/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DaynamicPage = () => {
+    return (
+        <div>
+            <h1> this is daynamic page content</h1>
+        </div>
+    );
+};
+
+export default DaynamicPage;
